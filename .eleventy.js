0fbb4970d7a94e31677c9492650a79c98f1b5938
Igor Korovchenko <igsekor@gmail.com>
2024-09-16 12:23:34 +0300

    Add 11ty basic config

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,33 @@
+const path = require('path')
+const EleventyVitePlugin = require('@11ty/eleventy-plugin-vite')
+
 module.exports = function (config) {
+
+  config.setServerOptions({
+
+  })
+
+  config.addPlugin(EleventyVitePlugin, {
+    viteOptions: {
+      appType: 'mpa',
+
+      server: {
+        mode: 'development',
+        middlewareMode: true,
+      },
+
+      build: {
+        mode: 'production',
+      },
+
+      resolve: {
+        alias: {
+          '/styles': path.resolve('.', 'src/styles')
+        }
+      }
+    }
+  })
+
   config.addPassthroughCopy('src/robots.txt')
   config.addPassthroughCopy('src/fonts')
 
