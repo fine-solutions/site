a36642ee2fadd4b66706b62169eeab02efac0583
Igor Korovchenko <igsekor@gmail.com>
2024-09-30 12:02:23 +0300

    Extract project set to common data file

--- src/views/index.11tydata.js
+++ src/views/index.11tydata.js
@@ -2,22 +2,6 @@ module.exports = {
   layout: 'base.njk',
   permalink: '/index.html',
   eleventyComputed: {
-    allProjects: function (data) {
-      const { collections } = data
-      const { projects } = collections
-      return projects
-      .map((p) => {
-          return {
-            title: p.data.title,
-            summary: p.data.summary,
-            progress: p.data.progress,
-            duration: p.data.duration,
-            src: `${p.filePathStem.replace(/index$/, 'covers')}/${p.data.covers.card.src}`,
-            alt: p.data.covers.card.alt,
-          }
-        })
-    },
-
     newProjects: function (data) {
       return data.allProjects
     },
