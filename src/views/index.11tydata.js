f8288614c95693f7a55575e095061ed9ad9e9d5c
Igor Korovchenko <igsekor@gmail.com>
2024-09-30 11:59:09 +0300

    Refactor project output

--- src/views/index.11tydata.js
+++ src/views/index.11tydata.js
@@ -1,4 +1,29 @@
 module.exports = {
   layout: 'base.njk',
   permalink: '/index.html',
+  eleventyComputed: {
+    allProjects: function (data) {
+      const { collections } = data
+      const { projects } = collections
+      return projects
+      .map((p) => {
+          return {
+            title: p.data.title,
+            summary: p.data.summary,
+            progress: p.data.progress,
+            duration: p.data.duration,
+            src: `${p.filePathStem.replace(/index$/, 'covers')}/${p.data.covers.card.src}`,
+            alt: p.data.covers.card.alt,
+          }
+        })
+    },
+
+    newProjects: function (data) {
+      return data.allProjects
+    },
+
+    successfulProjects: function (data) {
+      return data.allProjects
+    },
+  }
 }
